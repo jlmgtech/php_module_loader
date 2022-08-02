91278130b42f66615d24e4dfa76ea323926e0e6b
James Lay <james@eventrentalsystems.com>
2022-08-01 21:35:28 -0600

    create module manager and also render html in main.php

--- main.php
+++ main.php
@@ -1,19 +1,39 @@
 <?php
 
+/*
+ * TODO:
+ * - hooks and actions              loaded immediately              (done)
+ * - lib functions                  loaded on demand                (done)
+ * - navigation menu registration   loaded when menu fires ready    (easy)
+ * - ers core simulation            n/a                             (n/a)
+ * - url route registration         loaded during routing           (easy)
+ *      option 1: all routes under a subdomain call a routing function implemented on the module
+ *      option 2: routes under a subdomain are registered by the module calling route registration functions like get() or post()
+ * - rpc registration               loaded on demand I guess?       (hard)
+ * */
+
 require_once __DIR__ . "/" . "lib/load_modules.php";
+require_once __DIR__ . "/" . "lib/hook_functions.php";
 
 $safe_mode = [
-    "Core" => ["ExampleCore"],
-    "Billing" => ["Flubo"],
-    "Logging" => ["BasicLogger"],
+    "Core" => "ExampleCore",
+    "ModuleManager" => "ModuleManager",
+    "Billing" => "Flubo",
+    "Logging" => "BasicLogger",
 ];
 
 $user_mode = [
-    "Core" => ["ExampleCore"],
-    "Billing" => ["Infusionsoft", "ExampleBilling"],
-    "Logging" => ["HappyLogging", "BasicLogger"],
+    "Core" => "ExampleCore",
+    "ModuleManager" => "ModuleManager",
+    "Billing" => "Infusionsoft",
+    "Logging" => "BasicLogger", 
 ];
 
-$mode = $safe_mode;
+$mode = $user_mode;
 $loader = new ModuleLoader($mode);
-Core::render($loader);
+do_action("init");
+
+$code = ModuleManager::render($loader);
+$html = file_get_contents(__DIR__ . "/" . "index.tpl");
+$html = str_replace("[code]", $code, $html);
+echo $html;
--- modules/ModuleManager/ModuleManager/ModuleManager.php
+++ modules/ModuleManager/ModuleManager/ModuleManager.php
@@ -1,6 +1,6 @@
 <?php
 
-class ExampleCore {
+class ModuleManager {
 
     private static $menu = [];
 
