fcfdf0865630d5e9156b0b133c69193688f1c06f
james lay <james@eventrentalsystems.com>
2022-08-18 12:41:38 -0600

    add convenience method for menu registration

--- modules/AppMenu/AppMenu/AppMenu.php
+++ modules/AppMenu/AppMenu/AppMenu.php
@@ -2,6 +2,15 @@
 
 class AppMenu {
     public static $menu = [];
+
+    public static function add(string $icon) {
+        self::add_to_menu(
+            Actions::current_driver(),
+            AutoRouter::get(Actions::current_module(), "index"),
+            $icon
+        );
+    }
+
     public static function add_to_menu(string $name, string $url, string $icon) {
         self::$menu[$name] = [
             "url" => $url,
