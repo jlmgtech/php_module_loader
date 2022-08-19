4403b04b7d0622458432f7133f52a443efbe3aef
James Lay <james@eventrentalsystems.com>
2022-08-18 21:00:09 -0600

    chg links in AppMenu

--- modules/AppMenu/AppMenu/actions.php
+++ modules/AppMenu/AppMenu/actions.php
@@ -1,19 +1,15 @@
 <?php
 
 Actions::on("menu", function() {
-    AppMenu::add_to_menu(
-        Actions::current_driver(),
-        AutoRouter::get(Actions::current_module(), "index"),
-        "object-ungroup"
-    );
+    AppMenu::add("object-ungroup");
 });
 
 Actions::on("routes", function() {
-    Router::get("/admin", function() {
-        AutoRouter::go(Actions::current_module(), "index");
-    });
-    AutoRouter::set("index", "/", function() {
+    AutoRouter::set("index", "/", "Auth::login_guard", function() {
         include __DIR__ . "/" . "views/index.php";
+    });
+    Router::get("/admin", function() {
+        AutoRouter::go(Router::current_module(), "index");
     });
 });
 
